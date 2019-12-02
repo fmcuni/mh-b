c1cb5531d6afa60332bf46e45eaa72ac82eb588c
fmcuni <58059400+fmcuni@users.noreply.github.com>
2019-12-02 10:38:23 +0800

    Create mhktable.js

---
+++ mhktable.js
@@ -0,0 +1,1 @@
+document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://gitcdn.link/cdn/fmcuni/mhkb/3f9679fe6365e85bcb46c680566d8b95c95f68bd/mhktable.css" type="text/css"/>';
