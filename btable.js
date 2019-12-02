a844386c8027f1b7ee5204182aa64807bf56c189
fmcuni <58059400+fmcuni@users.noreply.github.com>
2019-12-02 14:09:55 +0800

    Update btable.js

--- btable.js
+++ btable.js
@@ -1,2 +1,2 @@
-document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://gitcdn.link/repo/fmcuni/mh-b/master/btable.css" type="text/css"/>';
-document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://gitcdn.link/repo/fmcuni/mh-b/master/font/icofont.min.css" type="text/css"/>';
+document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://combinatronics.com/fmcuni/mh-b/master/btable.css" type="text/css"/>';
+document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://combinatronics.com/fmcuni/mh-b/master/font/icofont.min.css" type="text/css"/>';
