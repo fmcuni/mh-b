955d8d2e9b662eeafcc3e365e33580a1d028c651
fmcuni <58059400+fmcuni@users.noreply.github.com>
2019-12-02 13:37:53 +0800

    Update btable.js

--- btable.js
+++ btable.js
@@ -1,1 +1,3 @@
 document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://gitcdn.link/repo/fmcuni/mh-b/master/btable.css" type="text/css"/>';
+document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://gitcdn.link/repo/fmcuni/mh-b/master/btable.css" type="text/css"/>';
+<link rel="stylesheet" href="myProjects/webProject/icofont/css/icofont.min.css">
