f33ac1d6c19e31be21070667c4f3a11ea30736b9
fmcuni <58059400+fmcuni@users.noreply.github.com>
2019-12-02 11:40:35 +0800

    Update and rename mhktable.js to btable.js

---
+++ btable.js
@@ -0,0 +1,1 @@
+document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://gitcdn.link/repo/fmcuni/mh-b/master/btable.css" type="text/css"/>';
--- mhktable.js
+++
@@ -1,1 +0,0 @@
-document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://gitcdn.link/repo/fmcuni/mhkb/master/mhktable.css" type="text/css"/>';
