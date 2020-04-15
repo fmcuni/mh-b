acaaa56fbef03532d98f9132a1552d3829a490f9
fmcuni <58059400+fmcuni@users.noreply.github.com>
2020-04-15 15:45:48 +0800

    Update btable.js

--- btable.js
+++ btable.js
@@ -1,2 +1,3 @@
 document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://combinatronics.com/fmcuni/mh-b/master/btable.css" type="text/css"/>';
 document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://gitcdn.link/repo/fmcuni/mh-b/master/font/icofont.min.css" type="text/css"/>';
+document.addEventListener("DOMContentLoaded", function(){var texttoget = $('div.editorial-code').text();$('div.editorial-code').attr('code-text', texttoget);});
