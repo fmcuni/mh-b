4081c07789ff14b1215206836d3bcc0ea68270a3
fmcuni <58059400+fmcuni@users.noreply.github.com>
2020-04-27 19:50:20 +0800

    Update btable.js

--- btable.js
+++ btable.js
@@ -2,7 +2,7 @@ document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https:
 document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://gitcdn.link/repo/fmcuni/mh-b/master/font/icofont.min.css" type="text/css"/>';
 document.querySelector('head').innerHTML += '<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700&display=swap" rel="stylesheet">';
 document.addEventListener("DOMContentLoaded", function() {
-
+    $(window).resize(customtableheaderwidth);
     // editorial code box
     $(".editorial-code-box").append('<a href="https://www.moneyhero.com.hk/zh/editorial-code" rel="noopener" target="_blank" style="display: block;"><div class="editorial-code">MoneyHero為第三方金融產品比較平台，相關內容與資訊僅以消費者的最佳利益角度出發。MoneyHero與金融機構合作設計最適合你的金融產品，但我們的編採團隊不受商業因素影響，始終保持獨立。</div></a>');
     var texttoget = $('div.editorial-code').text();
