4369168535e0646f96a6059c00dfd0ef2fa263f1
fmcuni <58059400+fmcuni@users.noreply.github.com>
2020-04-27 19:33:29 +0800

    Update btable.js

--- btable.js
+++ btable.js
@@ -2,14 +2,15 @@ document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https:
 document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://gitcdn.link/repo/fmcuni/mh-b/master/font/icofont.min.css" type="text/css"/>';
 document.querySelector('head').innerHTML += '<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700&display=swap" rel="stylesheet">';
 document.addEventListener("DOMContentLoaded", function() {
-    
+
     // editorial code box
     $(".editorial-code-box").append('<a href="https://www.moneyhero.com.hk/zh/editorial-code" rel="noopener" target="_blank" style="display: block;"><div class="editorial-code">MoneyHero為第三方金融產品比較平台，相關內容與資訊僅以消費者的最佳利益角度出發。MoneyHero與金融機構合作設計最適合你的金融產品，但我們的編採團隊不受商業因素影響，始終保持獨立。</div></a>');
     var texttoget = $('div.editorial-code').text();
     $('div.editorial-code').attr('code-text', texttoget);
-      
+
     // expandable table button
     $("button.expand-table").remove();
+
     function bindButtonEvt(t) {
         $('#table' + t + '.sticky').parent().after('<button id="table' + t + '-btn" class="expand-table closed" ><span>收合</span></button>');
         $('#table' + t + '-btn').off('click').click(function() {
@@ -20,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // fix sticky table width
     var numtablestickyth = [];
+
     function fixstickywidth(t) {
         $('#table' + t + '.sticky tbody:last-child').after('<tfoot><tr><td style="width: 999px;"></td></tr></tfoot>');
         $('#table' + t + '.sticky tbody:last-child').after('<tfoot><tr><td></td></tr></tfoot>');
@@ -37,22 +39,27 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         });
     }
-    
+
     // custom table header width
-	function customtableheaderwidth (t){
-    if ($(window).width() > 767) {
-    $('table' + t + '.mh-table th:nth-child(n+2)').css({"width":$('table' + t + '.mh-table').attr('col-width')});
+    function customtableheaderwidth(t) {
+        if ($(window).width() > 767) {
+            $('#table' + t + '.mh-table th:nth-child(n+2)').css({
+                "width": $('#table' + t + '.mh-table').attr('col-width')
+            });
+        } else {
+            $('#table' + t + '.mh-table th:nth-child(n+2)').css({
+                "min-width": $('#table' + t + '.mh-table').attr('mo-col-width')
+            });
+            $('#table' + t + '.mh-table th:first-child').css({
+                "min-width": $('#table' + t + '.mh-table').attr('mo-left-col-head-width')
+            });
+        }
     }
-    else {
-    $('table' + t + '.mh-table th:nth-child(n+2)').css({"min-width":$('table' + t + '.mh-table').attr('mo-col-width')});
-    $('table' + t + '.mh-table th:first-child').css({"min-width":$('table' + t + '.mh-table').attr('mo-left-col-head-width')});
-    }
-	}
-	//loop for 10 tables
-	for (var t = 1; t <= 10; t++) {
+    //loop for 10 tables
+    for (var t = 1; t <= 10; t++) {
         bindButtonEvt(t)
         searchabletable(t)
         fixstickywidth(t)
-		customtableheaderwidth(t)
+        customtableheaderwidth(t)
     }
 });
