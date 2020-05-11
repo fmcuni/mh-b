a57747ba2487468ef98d578443ab29fd0cc4ed9b
fmcuni <58059400+fmcuni@users.noreply.github.com>
2020-05-11 18:27:28 +0800

    Update btable.js

--- btable.js
+++ btable.js
@@ -8,6 +8,14 @@ if (window.isInited !== true) {
         $(".editorial-code-box").append('<a href="https://www.moneyhero.com.hk/zh/editorial-code" rel="noopener" target="_blank" style="display: block;"><div class="editorial-code">MoneyHero為第三方金融產品比較平台，相關內容與資訊僅以消費者的最佳利益角度出發。MoneyHero與金融機構合作設計最適合你的金融產品，但我們的編採團隊不受商業因素影響，始終保持獨立。</div></a>');
         var texttoget = $('div.editorial-code').text();
         $('div.editorial-code').attr('code-text', texttoget);
+        
+				// link on tip
+        $('.link-on-tip').each(function() {
+            var lnk = $(this).attr('href');
+            $(this).attr('title', lnk);
+        });
+        $('[data-toggle="tooltip"]').tooltip();
+        $('.link-on-tip').tooltip('enable');
     });
     document.addEventListener("DOMContentLoaded", function() {
         
