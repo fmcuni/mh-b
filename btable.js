25185c9b8055c9e3c81bca077b4d26de9fd2e636
fmcuni <58059400+fmcuni@users.noreply.github.com>
2020-05-13 15:20:43 +0800

    Loaded

--- btable.js
+++ btable.js
@@ -1,8 +1,8 @@
-if (window.isInited !== true) {
-    window.isInited = true
     document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://combinatronics.com/fmcuni/mh-b/master/btable.css" type="text/css"/>';
     document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://gitcdn.link/repo/fmcuni/mh-b/master/font/icofont.min.css" type="text/css"/>';
     document.querySelector('head').innerHTML += '<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700&display=swap" rel="stylesheet">';
+if (window.isInited !== true) {
+    window.isInited = true
     document.addEventListener("DOMContentLoaded", function() {
         // editorial code box
         $(".editorial-code-box").append('<a href="https://www.moneyhero.com.hk/zh/editorial-code" rel="noopener" target="_blank" style="display: block;"><div class="editorial-code">MoneyHero為第三方金融產品比較平台，相關內容與資訊僅以消費者的最佳利益角度出發。MoneyHero與金融機構合作設計最適合你的金融產品，但我們的編採團隊不受商業因素影響，始終保持獨立。</div></a>');
--- full-page-layout.js
+++ full-page-layout.js
@@ -1,4 +1,1 @@
-if (window.isInited !== true) {
-    window.isInited = true
     document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://combinatronics.com/fmcuni/mh-b/master/full-page-layout.css" type="text/css"/>';
-}
