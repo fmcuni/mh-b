681956d6b71d628acc1ebbce00401e777ecf45bc
fmcuni <58059400+fmcuni@users.noreply.github.com>
2020-05-13 14:50:36 +0800

    Create full-page-layout.js

---
+++ full-page-layout.js
@@ -0,0 +1,4 @@
+if (window.isInited !== true) {
+    window.isInited = true
+    document.querySelector('head').innerHTML += '<link rel="stylesheet" href="https://combinatronics.com/fmcuni/mh-b/master/full-page-layout.css" type="text/css"/>';
+}
