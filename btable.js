18e4e26d798a84c76c2e49827cb4350f0c18ce10
fmcuni <58059400+fmcuni@users.noreply.github.com>
2020-08-14 16:47:35 +0800

    Update btable.js

--- btable.js
+++ btable.js
@@ -1,3 +1,3 @@
-var imported = document.createElement('script');
-imported.src = 'https://cdn.jsdelivr.net/npm/moneyhero-wp-global@latest/dist/global.min.js';
-document.head.appendChild(imported);
+var globalJS = document.createElement('script');
+globalJS.src = 'https://cdn.jsdelivr.net/npm/moneyhero-wp-global@latest/dist/global.min.js';
+document.head.appendChild(globalJS);
